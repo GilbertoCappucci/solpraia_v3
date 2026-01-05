a00953a5323ea9353831c795538aa2ffb8595940
Gilberto Cappucci Junior <gilbertocappucci@gmail.com>
2026-01-05 16:18:45 -0300

    feat: Update UserFactory and UserSeeder to use consistent password and email verification logic

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,14 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
-
         // Create admin
         User::factory()->create([
             'id' => 1,
             'user_id' => 1,
             'name' => 'User admin',
             'email' => 'sol@gmail.com',
+            'password' => '45rtfgvb',
+            'email_verified_at' => now(),
             'remember_token' => null,
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
@@ -30,12 +30,13 @@ class UserSeeder extends Seeder
             'active' => true,
         ]);
 
-        //Create device 001
+        //User 1 normal
         User::factory()->create([
             'id' => 2,
             'user_id' => 1,
-            'name' => 'Device 001',
+            'name' => 'User normal',
             'email' => 'sol1@gmail.com',
+            'password' => '45rtfgvb',
             'email_verified_at' => now(),
             'password' => '45rtfgvb',
             'remember_token' => null,
@@ -46,14 +47,14 @@ class UserSeeder extends Seeder
             'active' => true,
         ]);
 
-        //Create device 002
+        //Create user 2 normal
         User::factory()->create([
             'id' => 3,
             'user_id' => 1,
-            'name' => 'Device 002',
+            'name' => 'User normal 2',
             'email' => 'sol2@gmail.com',
-            'email_verified_at' => now(),
             'password' => '45rtfgvb',
+            'email_verified_at' => now(),
             'remember_token' => null,
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
@@ -62,22 +63,22 @@ class UserSeeder extends Seeder
             'active' => true,
         ]);
 
-        // Create device 003
+        // Create admin 2
         User::factory()->create([
             'id' => 4,
-            'user_id' => 1,
-            'name' => 'Device 003',
-            'email' => 'sol3@gmail.com',
-            'email_verified_at' => now(),
+            'user_id' => 4,
+            'name' => 'User admin 2',
+            'email' => 'admin2@gmail.com',
             'password' => '45rtfgvb',
+            'email_verified_at' => now(),
             'remember_token' => null,
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
-            'role' => RoleEnum::DEVICE->value,
+            'role' => RoleEnum::ADMIN->value,
             'active' => true,
         ]);
-        
+
         /*
         for ($i = 0; $i < 2; $i++){
             User::factory()->create();
